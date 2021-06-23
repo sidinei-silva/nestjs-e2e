47ed51af723d539df5b9cff7e40d222dd84423ac
Sidinei Silva <sidinei.silva02@gmail.com>
2021-06-23 19:03:54 -0300

    Incluindo banco de dados
    Incluindo banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
